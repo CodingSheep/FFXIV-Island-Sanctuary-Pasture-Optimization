c2ddf406a5c814fbf5b312e1b86c6a8a8a17c65c
CodingSheep <jarrodfraine@me.com>
2023-02-07 13:40:54 -0700

    Moved necessary code to repo.

---
+++ constants.py
@@ -0,0 +1,21 @@
+rares = ['Alligator', 'Apkallu of Paradise', 'Beachcomb', 'Black Chocobo', 'Dodo of Paradise',
+         'Glyptodon', 'Goobue', 'Gold Back', 'Grand Buffalo', 'Island Billy', 'Island Stag',
+         'Lemur', 'Ornery Karakul', 'Paissa', 'Star Marmot', 'Twinklefleece', 'Yellow Coblyn',
+         'Griffin', 'Tiger', 'Morbol']
+
+replace = {
+    'Beachcomb': 'Beachcomb/Glyptodon Pup',
+    'Glyptodon Pup': 'Beachcomb/Glyptodon Pup',
+    'Lemur' : 'Lemur/Star Marmot',
+    'Star Marmot': 'Lemur/Star Marmot',
+    'Ground Squirrel': 'Ground Squirrel/Opo-Opo',
+    'Opo-Opo': 'Ground Squirrel/Opo-Opo',
+    'Aurochs': 'Aurochs/Island Nanny',
+    'Island Nanny': 'Aurochs/Island Nanny',
+    'Dodo of Paradise': 'Dodo of Paradise/Gold Back',
+    'Gold Back': 'Dodo of Paradise/Gold Back',
+    'Blue Back': 'Blue Back/Dodo',
+    'Dodo': 'Blue Back/Dodo',
+    'Yellow Coblyn': 'Yellow Coblyn/Morbol',
+    'Morbol': 'Yellow Coblyn/Morbol'
+}
