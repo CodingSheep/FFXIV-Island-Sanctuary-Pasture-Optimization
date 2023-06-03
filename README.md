4c5cee4ff640528d2a282ee1ef4308a27231d115
CodingSheep <jarrodfraine@me.com>
2023-06-02 23:06:23 -0600

    Optimized and Updated V2 code into V3. Added automated Base Yield and Bonus Yield Calculations. Removed Depreciated csvs due to Leavings Requirements now being based on Simulations.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 The purpose of these notebooks is to optimize what 20 animals to choose for your Pasture in FFXIV's Island Sanctuary content to keep up with Workshop demands. This code was developed with aid from the [Overseas Casuals](https://discord.gg/overseascasuals) Discord server.
 
 This is a personal project and thus is not likely to be optimized for general use.
+
+This code is updated to reflect Patch 6.4 changes.
--- constants.py
+++ constants.py
@@ -1,7 +1,8 @@
 rares = ['Alligator', 'Apkallu of Paradise', 'Beachcomb', 'Black Chocobo', 'Dodo of Paradise',
          'Glyptodon', 'Goobue', 'Gold Back', 'Grand Buffalo', 'Island Billy', 'Island Stag',
          'Lemur', 'Ornery Karakul', 'Paissa', 'Star Marmot', 'Twinklefleece', 'Yellow Coblyn',
-         'Griffin', 'Tiger', 'Morbol']
+         'Griffin', 'Tiger', 'Morbol', 'Amethyst Spriggan', 'Rare Boar', 'Weird Spriggan',
+         'Funguar', 'Alkonost']
 
 replace = {
     'Beachcomb': 'Beachcomb/Glyptodon Pup',
@@ -12,10 +13,16 @@ replace = {
     'Opo-Opo': 'Ground Squirrel/Opo-Opo',
     'Aurochs': 'Aurochs/Island Nanny',
     'Island Nanny': 'Aurochs/Island Nanny',
-    'Dodo of Paradise': 'Dodo of Paradise/Gold Back',
-    'Gold Back': 'Dodo of Paradise/Gold Back',
+    'Dodo of Paradise': 'Dodo of Paradise/Gold Back/Alkonost',
+    'Gold Back': 'Dodo of Paradise/Gold Back/Alkonost',
+    'Alkonost': 'Dodo of Paradise/Gold Back/Alkonost',
     'Blue Back': 'Blue Back/Dodo',
     'Dodo': 'Blue Back/Dodo',
     'Yellow Coblyn': 'Yellow Coblyn/Morbol',
-    'Morbol': 'Yellow Coblyn/Morbol'
+    'Morbol': 'Yellow Coblyn/Morbol',
+    'Tiger': 'Tiger/Quartz Spriggan/Weird Spriggan',
+    'Quartz Spriggan': 'Tiger/Quartz Spriggan/Weird Spriggan',
+    'Weird Spriggan': 'Tiger/Quartz Spriggan/Weird Spriggan',
+    'Twinklefleece': 'Twinklefleece/Funguar',
+    'Funguar': 'Twinklefleece/Funguar'
 }
