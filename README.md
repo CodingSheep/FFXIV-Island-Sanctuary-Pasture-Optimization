e881c7c740caf1a4e5fbbf8f3f1eebe9e461b034
CodingSheep <jarrodfraine@me.com>
2023-11-30 16:50:40 -0700

    Updated for 6.5 fully. Minor optimizations made using Itertools.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # FFXIV-Island-Sanctuary-Pasture-Optimization
 
-The purpose of these notebooks is to optimize what 20 animals to choose for your Pasture in FFXIV's Island Sanctuary content to keep up with Workshop demands. This code was developed with aid from the [Overseas Casuals](https://discord.gg/overseascasuals) Discord server.
+The purpose of these notebooks is to optimize what animals to choose for your Pasture in FFXIV's Island Sanctuary content to keep up with Workshop demands. This code was developed with aid from the [Overseas Casuals](https://discord.gg/overseascasuals) Discord server.
 
 This is a personal project and thus is not likely to be optimized for general use.
 
-This code is updated to reflect Patch 6.4 changes.
+This code is updated to reflect Patch 6.5 changes.
--- constants.py
+++ constants.py
@@ -2,27 +2,50 @@ rares = ['Alligator', 'Apkallu of Paradise', 'Beachcomb', 'Black Chocobo', 'Dodo
          'Glyptodon', 'Goobue', 'Gold Back', 'Grand Buffalo', 'Island Billy', 'Island Stag',
          'Lemur', 'Ornery Karakul', 'Paissa', 'Star Marmot', 'Twinklefleece', 'Yellow Coblyn',
          'Griffin', 'Tiger', 'Morbol', 'Amethyst Spriggan', 'Rare Boar', 'Weird Spriggan',
-         'Funguar', 'Alkonost']
+         'Funguar', 'Alkonost', 'Adamantoise', 'Pteranodon', 'Morbol', 'Grand Doblyn']
 
 replace = {
-    'Beachcomb': 'Beachcomb/Glyptodon Pup',
-    'Glyptodon Pup': 'Beachcomb/Glyptodon Pup',
+    # Carapace/Claw
+    'Beachcomb': 'Beachcomb/Glyptodon Pup/Adamantoise',
+    'Glyptodon Pup': 'Beachcomb/Glyptodon Pup/Adamantoise',
+    'Adamantoise': 'Beachcomb/Glyptodon Pup/Adamantoise',
+    
+    # Fur/Claw
     'Lemur' : 'Lemur/Star Marmot',
     'Star Marmot': 'Lemur/Star Marmot',
+    
+    # Claw/Fur
     'Ground Squirrel': 'Ground Squirrel/Opo-Opo',
     'Opo-Opo': 'Ground Squirrel/Opo-Opo',
+    
+    # Milk/Horn
     'Aurochs': 'Aurochs/Island Nanny',
     'Island Nanny': 'Aurochs/Island Nanny',
+    
+    # Feather/Egg
     'Dodo of Paradise': 'Dodo of Paradise/Gold Back/Alkonost',
     'Gold Back': 'Dodo of Paradise/Gold Back/Alkonost',
     'Alkonost': 'Dodo of Paradise/Gold Back/Alkonost',
+    
+    # Egg/Feather
     'Blue Back': 'Blue Back/Dodo',
     'Dodo': 'Blue Back/Dodo',
-    'Yellow Coblyn': 'Yellow Coblyn/Morbol',
-    'Morbol': 'Yellow Coblyn/Morbol',
+    
+    # Carapace/Fang
+    'Yellow Coblyn': 'Yellow Coblyn/Morbol Seedling',
+    'Morbol Seedling': 'Yellow Coblyn/Morbol Seedling',
+    
+    # Fang/Carapace
+    'Coblyn': 'Coblyn/Morbol/Grand Doblyn',
+    'Morbol': 'Coblyn/Morbol/Grand Doblyn',
+    'Grand Doblyn': 'Coblyn/Morbol/Grand Doblyn',
+    
+    # Fang/Fur
     'Tiger': 'Tiger/Quartz Spriggan/Weird Spriggan',
     'Quartz Spriggan': 'Tiger/Quartz Spriggan/Weird Spriggan',
     'Weird Spriggan': 'Tiger/Quartz Spriggan/Weird Spriggan',
+    
+    # Fleece/Fur
     'Twinklefleece': 'Twinklefleece/Funguar',
     'Funguar': 'Twinklefleece/Funguar'
 }
